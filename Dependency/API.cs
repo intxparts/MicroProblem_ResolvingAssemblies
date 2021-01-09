9d56ee81040a214f15f991f373299a2d31974ff7
intxparts <4543613+intxparts@users.noreply.github.com>
2021-01-09 12:13:01 -0500

    add example of resolving assemblies using privateBinPath

---
+++ Dependency/API.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dependency
+{
+    public class API
+    {
+        public static int Sum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
---
+++ WPFApplication/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Dependency;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            int i = API.Sum(2, 3);
+
+            base.OnStartup(e);
+        }
+    }
+}
